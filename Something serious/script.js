65355e1b31693971b4a1dd5d2347aadfc2fe0550
aabbdd1144 <abdgaming321@gmail.com>
2025-02-04 18:49:51 +0500

    Add files via upload

---
+++ Something serious/script.js
@@ -0,0 +1,5 @@
+document.getElementById("yesBtn").onclick = function() {
+  alert("You're my Valentine now! 💖");
+  document.body.style.background = "url('images/celebration_image.png')";
+  document.body.innerHTML = "<h1>Thank you for being mine forever!</h1><p><a href='https://open.spotify.com/track/0yc6Gst2xkRu0eMLeRMGCX?si=56f876f8b4174fbd'>Listen to our song 💕</a></p>";
+};
